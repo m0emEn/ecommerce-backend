ca1c03371cd62a3c9dc10193fc77427521f55530
m0emEn <95587718+m0emEn@users.noreply.github.com>
2024-05-19 18:58:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,24 +5,12 @@ import express from "express";
 import cors from "cors";
 import { productRoutes } from "./routes/productRoutes.js";
 import path from "path";
-const { createProxyMiddleware } = require("http-proxy-middleware");
 
 connectToDataBase();
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = 5000;
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "https://ecommerce-backend-wfya.onrender.com",
-    changeOrigin: true,
-  })
-);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 
 app.use("/api/products", productRoutes);
 
